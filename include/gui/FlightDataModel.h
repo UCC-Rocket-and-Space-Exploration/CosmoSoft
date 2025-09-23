fa5c2ccc479a9bdba826c8205def062bacd44012
mark <mark@mcannavan.dev>
2025-09-23 19:11:41 +0100

    Added initial directory structure & empty headers + cpp files

---
+++ include/gui/FlightDataModel.h
@@ -0,0 +1,13 @@
+//
+// Created by mark on 23.09.25.
+//
+
+#ifndef COSMO_SOFT_FLIGHTDATAMODEL_H
+#define COSMO_SOFT_FLIGHTDATAMODEL_H
+
+
+class FlightDataModel {
+};
+
+
+#endif //COSMO_SOFT_FLIGHTDATAMODEL_H
---
+++ include/persistence/FileManager.h
@@ -0,0 +1,13 @@
+//
+// Created by mark on 23.09.25.
+//
+
+#ifndef COSMO_SOFT_FILEMANAGER_H
+#define COSMO_SOFT_FILEMANAGER_H
+
+
+class FileManager {
+};
+
+
+#endif //COSMO_SOFT_FILEMANAGER_H
