fdd8585a1d4a5cd3167e3b420b782440e37b3a27
mark <mark@mcannavan.dev>
2026-02-17 17:27:46 +0000

    fixed minor errors in SerialCommsPosix.cpp

--- include/services/comms/SerialWorker.h
+++ include/services/comms/SerialWorker.h
@@ -2,16 +2,19 @@
 #define COSMO_SOFT_SERIALWORKER_H
 #include <functional>
 #include <thread>
+
+#include "../../domain/FlightSample.h"
 #include "../../gateway/IComms.h"
-#include "../../comms/ISerialPortScanner.h"
+#include "../../gateway/ISerialPortScanner.h"
 
 //To avoid blocking the UI when fetching serial data, we need to use Qt's signals and slots technique for async data fetching
+//TODO complete refactor; make async if possible, include error handling & logging, parsing, etc.
 class SerialWorker {
 public:
     explicit SerialWorker(IComms* comms);
     ~SerialWorker();
 
-    using DataCallback = std::function<void(const std::vector<uint8_t>&)>;
+    using DataCallback = std::function<void(std::vector<FlightSample>)>;
     using ErrorCallback = std::function<void(const std::string&)>;
 
     bool start();
--- include/services/telemetry/TelemetryParser.h
+++ include/services/telemetry/TelemetryParser.h
@@ -1,8 +1,17 @@
 #ifndef COSMO_SOFT_TELEMETRYPARSER_H
 #define COSMO_SOFT_TELEMETRYPARSER_H
+#include <cstdint>
+#include <vector>
 
+//TODO turn into producer-consumer pair
 class TelemetryParser {
-
+public:
+    TelemetryParser(&is_empty);
+    // serial input - > block output
+    // handle errors?
+    // start - end frame denoters
+private:
+    std::vector<std::uint8_t> m_buf;
 };
 
 #endif //COSMO_SOFT_TELEMETRYPARSER_H
