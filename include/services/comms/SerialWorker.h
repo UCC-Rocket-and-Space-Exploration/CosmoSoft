d8bb6785942b973e5833c79d7e208df727a3a29e
mark <mark@mcannavan.dev>
2026-02-18 19:55:53 +0000

    added header for Framer and Parser (+ ring buffer for inter-thread communication)

--- include/services/comms/SerialWorker.h
+++ include/services/comms/SerialWorker.h
@@ -1,5 +1,6 @@
 #ifndef COSMO_SOFT_SERIALWORKER_H
 #define COSMO_SOFT_SERIALWORKER_H
+#include <atomic>
 #include <functional>
 #include <thread>
 
@@ -9,6 +10,7 @@
 
 //To avoid blocking the UI when fetching serial data, we need to use Qt's signals and slots technique for async data fetching
 //TODO complete refactor; make async if possible, include error handling & logging, parsing, etc.
+// - Also rewrite to use std::jthread and std::stop_token for better thread management and cancellation
 class SerialWorker {
 public:
     explicit SerialWorker(IComms* comms);
--- include/services/telemetry/ParserWorker.h
+++ include/services/telemetry/ParserWorker.h
@@ -1,8 +1,34 @@
 #ifndef COSMO_SOFT_PARSERWORKER_H
 #define COSMO_SOFT_PARSERWORKER_H
+#include <atomic>
+#include <functional>
+#include <string>
+#include <thread>
+
+#include "../../domain/FlightSample.h"
 
 class ParserWorker {
+public:
+    using DataCallback = std::function<void(std::vector<FlightSample>)>;
+    using ErrorCallback = std::function<void(const std::string&)>;
 
+    explicit ParserWorker(DataCallback dataCallback, ErrorCallback errorCallback);
+    ~ParserWorker();
+
+    bool start();
+    void stop();
+
+    void setDataCallback(DataCallback callback);
+    void setErrorCallback(ErrorCallback callback);
+
+private:
+    void run();
+
+    std::thread m_workerThread;
+    std::atomic<bool> m_running;
+    DataCallback m_onData;
+    ErrorCallback m_onError;
 };
+
 
 #endif //COSMO_SOFT_PARSERWORKER_H
