e5e052a48dd3d95ca89a90736ba62dc7ce88d6ca
mark <mark@mcannavan.dev>
2026-03-04 18:32:50 +0000

    implemented CommsFactory for linux

--- src/gateway/comms/CommsFactory.cpp
+++ src/gateway/comms/CommsFactory.cpp
@@ -1,2 +1,14 @@
 #include "../../../include/gateway/CommsFactory.h"
 
+#include "gateway/posix/SerialCommsPosix.h"
+
+
+#ifdef _WIN32
+//TODO implement windows version of SerialComms and include here
+#elif __unix__
+std::unique_ptr<IComms> CommsFactory::createSerialComms(const std::string& device, int baud) {
+    return std::make_unique<SerialCommsPosix>(device, baud);
+}
+#else
+#error "Unsupported platform"
+#endif
