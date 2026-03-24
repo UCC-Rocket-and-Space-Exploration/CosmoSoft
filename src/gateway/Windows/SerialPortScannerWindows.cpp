d5127456241c1a1a06d8f5033ee7d0360883402d
Ginessis <106706657+GinessisC@users.noreply.github.com>
2026-03-24 19:37:17 +0100

    unnecessary comments deleted

--- src/gateway/Windows/SerialPortScannerWindows.cpp
+++ src/gateway/Windows/SerialPortScannerWindows.cpp
@@ -1,10 +1,5 @@
 #include "gateway/comms/windows/SerialPortScannerWindows.h"
 
-
-// #include "comms/SerialPortScannerWindows.h"
-
-#include "gateway/comms/windows/SerialPortScannerWindows.h"
-
 #include <string>
 #include <vector>
 #include <windows.h>
@@ -16,10 +11,8 @@ std::vector<std::string> SerialPortScannerWindows::enumeratePorts() {
     const char* subKey = "HARDWARE\\DEVICEMAP\\SERIALCOMM";
 
     if (RegOpenKeyExA(HKEY_LOCAL_MACHINE, subKey, 0, KEY_READ, &hKey) != ERROR_SUCCESS) {
-        // cout << "Failed to open key" << endl;
         throw "Failed to open key";
     }
-    // ports.push_back("\\\\.\\COM1");
     DWORD index = 0;
     char valueName[256];
     BYTE data[256];
@@ -42,7 +35,6 @@ std::vector<std::string> SerialPortScannerWindows::enumeratePorts() {
             break;
         if (result == ERROR_SUCCESS) {
             ports.push_back((char*)data);
-            // cout << "Val: " << valueName << " DATA: " << (char*)data << endl;
         }
         else {
             RegCloseKey(hKey);
