a06af018a6e341525af09f7b39f8940ef2bfb0db
Slowlybomb <hslyusar@redhat.com>
2026-04-21 22:54:23 +0100

    Update CMake configuration and enhance build management
    - Updated CMakeCache.txt to reflect changes in the C++ compiler path and version.
    - Incremented CMake cache patch version for better version tracking.
    - Added new Qt features in CMakeCache.txt to support additional functionalities.
    - Updated QtDeploySupport.cmake to reference the latest Qt6CoreDeploySupport.cmake file.
    - Adjusted Makefile dependencies to align with the new CMake version and structure.
    - Refined build rules for various targets to improve build efficiency and clarity.

--- build/.qt/QtDeploySupport.cmake
+++ build/.qt/QtDeploySupport.cmake
@@ -61,7 +61,7 @@ set(__QT_DEPLOY_QT_DEBUG_POSTFIX "")
 # Define the CMake commands to be made available during deployment.
 set(__qt_deploy_support_files
     "/Users/hslyusar/Desktop/CosmoSoft/build/.qt/QtDeployTargets.cmake"
-    "/opt/homebrew/Cellar/qtbase/6.10.2/lib/cmake/Qt6Core/Qt6CoreDeploySupport.cmake"
+    "/opt/homebrew/Cellar/qtbase/6.11.0/lib/cmake/Qt6Core/Qt6CoreDeploySupport.cmake"
 )
 foreach(__qt_deploy_support_file IN LISTS __qt_deploy_support_files)
     include("${__qt_deploy_support_file}")
