a5aaf9bd9c9518eda2a3b38795e9b7ba73a58446
Ginessis <106706657+GinessisC@users.noreply.github.com>
2026-08-23 17:34:17 +0200

    logging added to workers

--- include/services/telemetry/ParserWorker.h
+++ include/services/telemetry/ParserWorker.h
@@ -7,14 +7,21 @@
 #include "shared/abstraction/Worker.h"
 #include "domain/FlightSample.h"
 #include "services/RingBuffer.h"
+#include "services/interfaces/ILogger.h"
 
 #define Buffer std::shared_ptr<IBuffer<Frame>>
 
 class ParserWorker : public Worker{
     public:
     explicit ParserWorker(
-        const Buffer& buffer,
-        void (*on_parsed_data_callback)(const FlightSample& flight_sample));
+            const Buffer& buffer,
+            void (*on_parsed_data_callback)(const FlightSample& flight_sample),
+            const std::shared_ptr<ILogger>& debug_logger) {
+        m_on_parsed_data_callback = on_parsed_data_callback;
+        m_buffer = buffer;
+        m_decoder_vault = FrameDecoderVault();
+        m_debug_logger = debug_logger;
+    }
     ~ParserWorker() override = default;
 protected:
     void m_process() override;
@@ -23,7 +30,7 @@ private:
     FrameDecoderVault m_decoder_vault;
     void (*m_on_parsed_data_callback)(const FlightSample& flight_sample);
     Buffer m_buffer;
-
+    std::shared_ptr<ILogger> m_debug_logger;
     std::atomic_bool m_first_frame {true};
     int m_initial_timestamp = 0;
 };
--- src/services/telemetry/ParserWorker.cpp
+++ src/services/telemetry/ParserWorker.cpp
@@ -3,29 +3,23 @@
 #include "shared/exceptions/IncorrectAltosPacketType.h"
 
 
-ParserWorker::ParserWorker(
-        const Buffer& buffer,
-        void (*on_parsed_data_callback)(const FlightSample& flight_sample)) {
-    m_on_parsed_data_callback = on_parsed_data_callback;
-    m_buffer = buffer;
-    m_decoder_vault = FrameDecoderVault();
-}
 
 
 void ParserWorker::m_process() {
     while (m_running) {
         std::optional<Frame> frame = m_buffer->get();
+
         if (frame.has_value()) {
             std::shared_ptr<IFrameDecoder> decoder;
             try {
                 decoder = m_decoder_vault.select(frame.value());
             }
             catch (std::exception& e) {
-                std::cerr << e.what() << "\n";
+                m_debug_logger->LogLine(e.what());
                 continue;
             }
             FlightSample f_sample = decoder->decode(frame.value());
-            std::cout << "decoded frame timestamp: " << f_sample.timestamp << std::endl;
+
             if (m_first_frame) {
                 m_initial_timestamp = f_sample.timestamp;
                 m_first_frame = false;
